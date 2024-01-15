bedc19bd9547c3d5d57ed28616a18111ce409a84
TheDeveloperAlex <scorpion422@ukr.net>
2024-01-15 20:59:44 +0100

    add footer

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,21 +1,40 @@
 import React from "react";
+
+import Divider from "@mui/material/Divider";
 import { socials } from "./footer.data";
+
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer>
-      <ul>
-        {socials.map(({ name, link, image }) => {
-          return (
-            <li key={name}>
-              <div>
-                {/*  */}
-                {image}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+    <footer className="footer">
+      <div className="footer-content">
+        <Divider
+          orientation="horizontal"
+          variant="fullWidth"
+          sx={{
+            borderColor: "var(--charcoal-color)",
+          }}
+          className={"footer-divider"}
+        />
+        <ul className="footer-list">
+          {socials.map(({ name, link, image }) => {
+            return (
+              <li key={name}>
+                <a
+                  href={link}
+                  className={"footer-list-link"}
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  {image}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="pseudo-element-myProjects-circle"></div>
     </footer>
   );
 }
