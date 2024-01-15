fb131b0ef67f21d969e237be128b33423faa62af
TheDeveloperAlex <scorpion422@ukr.net>
2024-01-15 20:56:39 +0100

    fix header | add Hero | add root color

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { RefObject } from "react";
 import { IconArrow } from "../../components/icons";
 
-export default function Hero() {
+import "./Hero.css";
+
+interface HeroProps {
+  refAdvantages: RefObject<HTMLSelectElement> | null;
+}
+
+export default function Hero({ refAdvantages }: HeroProps) {
+  const scrollToAdvantages = () => {
+    if (refAdvantages) {
+      refAdvantages.current?.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <article className="container">
-      <h1>
-        <span>ALEX SEMENENKO.</span>
-        <span>
-          FULLSTACK <span>DEVELOPER</span>
-        </span>
+    <article className="container hero-section">
+      <h1 className="hero-header">
+        <p className="hero-header-name">ALEX SEMENENKO</p>
+        <p className="hero-header-fullstack">
+          <span>FULLSTACK </span>
+          <span className="hero-header-developer">
+            DEVELOPER
+            <span className="hero-header-developer-span hero-header-developer-span-top-left"></span>
+            <span className="hero-header-developer-span hero-header-developer-span-top-right"></span>
+            <span className="hero-header-developer-span hero-header-developer-span-bottom-right"></span>
+            <span className="hero-header-developer-span hero-header-developer-span-bottom-left"></span>
+          </span>
+        </p>
       </h1>
-      <p>
-        Frontend Developer, 2+ years in React.js, Next.js, and Typescript for
-        high-performance JavaScript apps.
+      <p className="hero-content">
+        2+ years in React.js, Next.js, and Typescript for high-performance
+        JavaScript web-apps.
       </p>
-      <p>Look more</p>
-      <IconArrow />
+      <p className="hero-look-more">Look more</p>
+      <button className="hero-look-more-arrow" onClick={scrollToAdvantages}>
+        <IconArrow />
+      </button>
     </article>
   );
 }
